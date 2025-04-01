277cbbba3d9ab79c39b533da9d4a765c18e7ae66
nishantkumarr <nishanttdevops@gmail.com>
2025-04-01 10:06:46 +0000

    modified

--- Docker-install/main.tf
+++ Docker-install/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 # ✅ Security Group Creation
 resource "aws_security_group" "my_sg" {
-  name        = "nishant-security-group"
+  name        = "test-security-group"
   description = "Allow SSH and HTTP"
 
   ingress {
@@ -21,6 +21,13 @@ resource "aws_security_group" "my_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+ }
+ 
   egress {
     from_port   = 0
     to_port     = 0
@@ -47,8 +54,15 @@ resource "aws_instance" "instances" {
   }
 }
 
-# ✅ S3 Bucket Creation
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = var.bucket
-  acl    = "private"
-}
+ # Generate a random string for uniqueness
+ resource "random_string" "suffix" {
+   length  = 6
+   special = false
+   upper   = false
+ }
+
+ # Create an S3 bucket with a random suffix
+ resource "aws_s3_bucket" "my_bucket" {
+   bucket = "${var.bucket}-${random_string.suffix.result}"
+ }
+ 
