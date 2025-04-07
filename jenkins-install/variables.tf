c8538650d071de25b00459709cd4c0596087c64b
nishantkumarr <nishanttdevops@gmail.com>
2025-04-07 16:00:29 +0000

    update

---
+++ jenkins-install/variables.tf
@@ -0,0 +1,24 @@
+variable "instance_type" {
+    type = string
+    default = "t3.micro"
+}
+
+variable "key_name"{
+    type = string
+    default = "nishant"
+}
+
+variable "ami_id"{
+    type = string
+    default = "ami-0129865974a10c1cb"
+}
+
+variable "instance_name"{
+    type = string
+    default = "testing"
+}
+
+variable "region" {
+    type = string
+    default = "us-east-1"
+}
